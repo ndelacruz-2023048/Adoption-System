aa19278abd60a24371abf04b5a9b8f45f957a619
josuenoj <josuenoj@kinal.edu.gt>
2025-01-29 09:18:23 -0600

    Feat: first configurations (mongo, express, dotenv)

---
+++ configs/app.js
@@ -0,0 +1,36 @@
+//Levantar servidor express (HTTP)
+
+//Modular | + efectiva + legible | trabaja en funciones
+
+'use strict'
+
+//ECModules | ESModules
+import express from 'express' //Servidor HTTP
+import morgan from 'morgan' //Logs
+import helmet from 'helmet' //Seguridad para HTTP
+import cors from 'cors' //Acceso al API
+
+//Configuraciones de express
+const configs = (app)=>{
+    app.use(express.json()) //Aceptar y enviar datos en JSON
+    app.use(express.urlencoded({extended: false})) //No encriptar la URL
+    app.use(cors())
+    app.use(helmet())
+    app.use(morgan('dev'))
+}
+
+/* const rutes = (app)=>{
+    app.use('rutas de la entidad')
+} */
+
+//Ejecutarmos el servidor
+export const initServer = ()=>{
+    const app = express() //Instancia de express
+    try{
+        configs(app)
+        app.listen(process.env.PORT)
+        console.log(`Server running in port ${process.env.PORT}`)
+    }catch(err){
+        console.error('Server init failed', err)
+    }
+}
